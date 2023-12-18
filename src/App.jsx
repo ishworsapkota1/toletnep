3c56dcf0d38bb98d9705df0f50581a78c08e3fa8
Ukesh Shrestha <uks56shr567@gmail.com>
2023-12-18 14:57:15 +0545

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,48 @@ import Settings from "./assets/settings/Settings";
 import Layouts from "./assets/homefolder/Layouts";
 import Navbars from "./assets/dropdowns/Navbars";
 import NewKycChild from "./assets/settings/settingscomponents/editprofile/NewKycChild";
+import { createContext, useState } from "react";
+
+export const CartContext = createContext([])
+
+
+
 function App() {
+const [cart, setCart] = useState([])
+console.log(cart)
+const cartHelperFunction = {
+  addToCart: (item) => {
+    console.log('inside add')
+ const isItemInCart = cart.find((cartItem) => cartItem.name === item.name);
+
+  if (isItemInCart) {
+  setCart(
+      cart.map((cartItem) =>
+            cartItem.id === item.id
+          ? { ...cartItem, quantity: cartItem.quantity + 1 }
+          : cartItem
+      )
+  );
+  } else {
+  setCart([...cart, { ...item, quantity: 1 }]);
+  }
+
+  }
+}
   return (
     <div>
       {/* <Homes />; */}
       <Navbars />
+
+<CartContext.Provider value={{addToCart:cartHelperFunction.addToCart, cart }}>
+
       <Routes>
         <Route path="/" element={<Homes />}></Route>
         <Route path="/wishlist" element={<WishList />}></Route>
         <Route path="/settings" element={<Settings />}></Route>
         <Route path="/newkycchild" element={<NewKycChild />}></Route>
       </Routes>
+</CartContext.Provider>
       <Layouts />
     </div>
   );
